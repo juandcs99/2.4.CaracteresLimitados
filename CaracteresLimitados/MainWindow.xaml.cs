73b216e1be5a0f285c896aa01e99563fea792351
Juan Diego <jd.cabrera@iesdoctorbalmis.com>
2021-09-30 13:30:03 +0200

    cambio

--- CaracteresLimitados/MainWindow.xaml.cs
+++ CaracteresLimitados/MainWindow.xaml.cs
@@ -15,15 +15,11 @@ using System.Windows.Shapes;
 
 namespace CaracteresLimitados
 {
-    /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-
         }
 
         private void CambiarEdicionTexto()
